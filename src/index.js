e929fd68eaf78d5c456f0a271eda5aa87494b488
Tatiana Mironycheva <luchenkovatatyana@yandex.ru>
2024-11-21 22:36:42 +0100

    webpack babel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-const cardTemplate = document.querySelector('#card-template').content;
+import './pages/index.css';
+import { initialCards } from './scripts/cards.js';
 
+const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
 
 function createCard(name, link, deleteCard) {
